285a9a4bf0a5be71482be912d854ad661df63e96
Corey Lloyd <coreyjdl@gmail.com>
2021-05-05 19:56:14 -0500

    refactored pi code

---
+++ app.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+import serial, json, csv
+from datetime import datetime
+
+def parse_data(data):
+    return json.loads(data.decode('utf-8').strip())
+
+def main():
+
+    # the thumb drive has been troublesome
+    #file_path = '/media/pi/A4F8-E1DB/'
+    file_path = './'
+    date =  datetime.now().strftime('%m_%d_%y-%H_%M')
+    filename = f'SENSLOG-{date}.txt'
+
+    ser = serial.Serial("/dev/ttyACM0", 9600)
+
+    data = parse_data(ser.readline())  
+    fields = ['timestamp', *data.keys()]
+
+    with open(filename, 'w') as out_file:
+        writer = csv.DictWriter(out_file, fieldnames=fields)
+        writer.writeheader()
+
+        while(True):
+            incoming = ser.readline()
+            out = {"timestamp": datetime.now(), **parse_data(incoming)}
+            writer.writerow(out)
+
+if __name__ == '__main__':
+    main()
