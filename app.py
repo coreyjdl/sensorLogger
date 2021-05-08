50283e8f63167c67bfe846d0e465084351ae03e8
Corey Lloyd <coreyjdl@gmail.com>
2021-05-07 20:51:34 -0500

    minor work

--- app.py
+++ app.py
@@ -7,18 +7,14 @@ def parse_data(data):
     return json.loads(data.decode('utf-8').strip())
 
 def main():
-
-    # the thumb drive has been troublesome
-    #file_path = '/media/pi/A4F8-E1DB1/'
-    file_path = './'
     date =  datetime.now().strftime('%m_%d_%y-%H_%M')
     filename = f'SENSLOG-{date}.txt'
-    new_file = True
+    writer = None 
 
     ser = serial.Serial("/dev/ttyACM0", 9600)
 
     with open(filename, 'a') as out_file:
-        writer = None 
+        
         while(True):
             incoming = parse_data(ser.readline())
 
